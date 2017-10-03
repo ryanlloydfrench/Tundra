eef4b3a9c1aa9b1cae4e935a3f147cff5a1f43fd
Ryan French <ryan.lloyd.french@gmail.com>
2017-10-03 15:24:19 +1100

    Add all tiles with particles.

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,27 @@
-particlesJS.load('particles-js', '../particles.json', function() {
+particlesJS.load('particles-js-1', '../particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles-js-2', '../particles-line.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles-js-3', '../particles-line.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles-js-4', '../particles-line.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles-js-5', '../particles-line.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles-js-6', '../particles-line.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+particlesJS.load('particles-js-7', '../particles-line.json', function() {
   console.log('callback - particles.js config loaded');
 });
